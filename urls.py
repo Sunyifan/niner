45b7a450b85b2ece240ef8dbeca114c0ce2b694b
Sunyifan <sunyifan@baixing.com>
2016-02-21 18:02:36 +0800

    reformat

--- urls.py
+++ urls.py
@@ -1,17 +1,17 @@
 __author__ = 'magus0219'
+
 import handlers
 from tornado.web import RedirectHandler
 from core.deploy_manager import dms
 
 URLS = [
-            # Core Entity
-            (r'/event', handlers.DeployHandler),
-            (r'/repo/(.*)/rollback/(.*)/(.*)', handlers.RollbackHandler),
-            (r'/repo/(.*)/(.*)', handlers.OperationHandler),
-            (r'/repo/(.*)', handlers.IndexHandler),
-            (r'/repo', RedirectHandler, {"url": "/deploy/repo/{repo}".format(repo=list(dms.keys())[0])}),
-            (r'/login', handlers.LoginHandler),
-            (r'/logout', handlers.LogoutHandler),
-            (r'/register', handlers.RegisterHandler),
-            (r'/chpwd', handlers.ChangePasswordHandler)
-        ]
+    (r'/event', handlers.DeployHandler),
+    (r'/repo/(.*)/rollback/(.*)/(.*)', handlers.RollbackHandler),
+    (r'/repo/(.*)/(.*)', handlers.OperationHandler),
+    (r'/repo/(.*)', handlers.IndexHandler),
+    (r'/repo', RedirectHandler, {"url": "/deploy/repo/{repo}".format(repo=list(dms.keys())[0])}),
+    (r'/login', handlers.LoginHandler),
+    (r'/logout', handlers.LogoutHandler),
+    (r'/register', handlers.RegisterHandler),
+    (r'/chpwd', handlers.ChangePasswordHandler),
+]
